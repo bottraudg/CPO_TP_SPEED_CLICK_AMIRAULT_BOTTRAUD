e92acb697aedc33c8f200c520e518af18a044ee3
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-11-24 10:54:32 +0100

    début des classes

--- src/cpo_tp_speed_click_amirault_bottraud/GrilleDeJeu.java
+++ src/cpo_tp_speed_click_amirault_bottraud/GrilleDeJeu.java
@@ -9,5 +9,23 @@ package cpo_tp_speed_click_amirault_bottraud;
  * @author guilenebottraud
  */
 public class GrilleDeJeu {
+    private int nbLignes;
+    private int nbColonne;
+    
+    public void EteindreToutesLesCellules (){
+        for (int i =0; i<nbLignes ; i++){
+            for (int j=0; j<nbColonne; j++){
+                matriceCellules[i][j].eteindreCellule();
+            }
+        }
+    }
+    
+    public void ActiverUneCellule(){
+        for (int i=0; i<nbLignes ; i++){
+            for (int j=0; j<nbColonne; j++){
+                matriceCellules[i][j].allumerCellule()
+            }
+        }
+    }
     
 }
