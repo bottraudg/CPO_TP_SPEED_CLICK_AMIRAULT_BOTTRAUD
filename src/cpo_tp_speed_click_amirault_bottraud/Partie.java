387983c40d73604044340e5c9635da93e6d1404b
guilenebottraud <guilenebottraud@10.3.61.49>
2023-11-24 10:54:27 +0100

    debut de classe partie

--- src/cpo_tp_speed_click_amirault_bottraud/Partie.java
+++ src/cpo_tp_speed_click_amirault_bottraud/Partie.java
@@ -4,10 +4,28 @@
  */
 package cpo_tp_speed_click_amirault_bottraud;
 
+import java.util.Scanner;
+
 /**
  *
  * @author guilenebottraud
  */
 public class Partie {
-    
+
+    int difficulte;
+    Scanner scanner;
+    String niveau;
+
+    public Partie() {
+        System.out.println("Bienvenue dans le jeu Speed Click!");
+        scanner = new Scanner(System.in);
+        while (difficulte == 0) {
+            System.out.println("Choisisez un niveau ( facile/ moyen/ difficile ) : ");
+            
+            niveau = scanner.nextLine();
+            int nbCellule = 0;
+
+        }
+
+    }
 }
